e3344a890e28ad30add989e7f3ca11728f83bdb9
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2017-01-10 12:21:20 +0700

    sync script with new class name

--- js/resize_figure.js
+++ js/resize_figure.js
@@ -1,6 +1,6 @@
 $().ready(function() {
-  $.each($('div.post-image'), function(i, val) {
-    var img = $(val).find('img');
+  $.each($('div.post-image-container'), function(i, val) {
+    var img = $(val).find('img.post-image-img');
     img.imagesLoaded(function() {
       $(val).find('div.post-image-caption').css("maxWidth", img.get(0).naturalWidth);
     });
